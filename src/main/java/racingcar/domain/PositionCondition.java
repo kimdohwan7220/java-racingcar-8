aded9fce9281a516250e4d833ef0f1f0658794e7
KimDohwan <dohwan8220@naver.com>
2025-10-24 16:55:06 +0900

    refactor: PositionCondition 인터페이스 제거 및 DefaultPositionCondition 통합

--- src/main/java/racingcar/domain/PositionCondition.java
+++ src/main/java/racingcar/domain/PositionCondition.java
@@ -1,5 +1,17 @@
 package racingcar.domain;
 
-public interface PositionCondition {
-    boolean canMove();
+
+import java.util.function.Supplier;
+
+public class PositionCondition {
+    private static final int MOVE_THRESHOLD = 4;
+    private final Supplier<Integer> randomNumberSupplier;
+
+    public PositionCondition(Supplier<Integer> randomNumberSupplier) {
+        this.randomNumberSupplier = randomNumberSupplier;
+    }
+
+    public boolean canMove() {
+        return randomNumberSupplier.get() >= MOVE_THRESHOLD;
+    }
 }

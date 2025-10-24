82b79987e5cca305aabef19feb91a6faad642543
KimDohwan <dohwan8220@naver.com>
2025-10-24 17:30:47 +0900

    feat: 자동차 위치 관리용 Position 클래스 추가

--- src/main/java/racingcar/domain/Position.java
+++ src/main/java/racingcar/domain/Position.java
@@ -1,4 +1,17 @@
 package racingcar.domain;
 
 public class Position {
+    private int location;
+
+    public Position() {
+        this.location = 0;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void moveForward() {
+        location++;
+    }
 }

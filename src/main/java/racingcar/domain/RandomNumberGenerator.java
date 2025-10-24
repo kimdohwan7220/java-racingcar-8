d89fea3179f6ad3efebbdf892c1e6e7cd43f624c
KimDohwan <dohwan8220@naver.com>
2025-10-24 14:25:58 +0900

    feat: RandomNumberGenerator 구현

---
+++ src/main/java/racingcar/domain/RandomNumberGenerator.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 9;
+
+    public int generate() {
+        return Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE);
+    }
+}

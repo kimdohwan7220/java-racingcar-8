adaf6e0ecfbc656e473702d7c41914c523874778
kimdohwan7220 <dohwan8220@naver.com>
2025-10-26 07:10:35 +0900

    refactor: RandomNumberGenerator 패키지 위치를 domain에서 utils로 이동

--- src/main/java/racingcar/RacingController.java
+++ src/main/java/racingcar/RacingController.java
@@ -1,4 +1,12 @@
 package racingcar;
 
+import racingcar.view.InputView;
+
 public class RacingController {
+    private final InputView inputView;
+
+    public RacingController(InputView inputView) {
+        this.inputView = inputView;
+
+    }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class CarTest {
+class CarTest {
 
     @DisplayName("자동차 이름 생성 성공 테스트")
     @ParameterizedTest

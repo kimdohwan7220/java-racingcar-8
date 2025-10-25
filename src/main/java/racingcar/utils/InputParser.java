381b95e0b43d35fbba1e16f17e356dcaf5bc8259
kimdohwan7220 <dohwan8220@naver.com>
2025-10-26 07:08:42 +0900

    feat: 사용자 입력 및 쉼표 기준 파싱 추가

--- src/main/java/racingcar/utils/InputParser.java
+++ src/main/java/racingcar/utils/InputParser.java
@@ -1,4 +1,20 @@
 package racingcar.utils;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.domain.Name;
+
 public class InputParser {
+    public static List<Name> inputParseDelimiter(String input) {
+
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException("모든 자동차의 이름을 입력해주세요.");
+        }
+
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Name::new)
+                .collect(Collectors.toList());
+    }
 }

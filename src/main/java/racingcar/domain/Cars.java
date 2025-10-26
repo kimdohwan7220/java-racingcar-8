2fb06b2dca927fe37379ea12719e86335c89265a
kimdohwan7220 <dohwan8220@naver.com>
2025-10-26 10:49:39 +0900

    feat: Cars 클래스에서 자동차들의 이동 및 상태 구현

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,4 +1,37 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.utils.RandomNumberGenerator;
+
 public class Cars {
+    private final List<Car> cars;
+    private final PositionCondition positionCondition;
+
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+        this.positionCondition = new PositionCondition();
+    }
+
+    public void moveAllCars() {
+        for (Car car : cars) {
+            positionCondition.tryMove(car);
+        }
+    }
+
+    private int getMaxCarPosition() {
+        int max = 0;
+        for (Car car : cars) {
+            max = Math.max(max, car.getPosition());
+        }
+        return max;
+    }
+
+    public List<String> getCarsPosition() {
+        return cars.stream()
+                .map(Car::toString)
+                .collect(Collectors.toList());
+    }
+
 }

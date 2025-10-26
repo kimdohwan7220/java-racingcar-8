518fa6a22cf6f78a0cdf123282c3c119ee96c8c9
KimDohwan <dohwan8220@naver.com>
2025-10-26 15:29:41 +0900

    feat: 컨트롤러에서 Cars 객체 생성 및 게임 실행 로직 구현

--- src/main/java/racingcar/RacingController.java
+++ src/main/java/racingcar/RacingController.java
@@ -1,12 +1,46 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.domain.Cars;
+import racingcar.domain.Name;
+import racingcar.domain.TryCount;
+import racingcar.utils.InputParser;
+import racingcar.utils.RandomNumberGenerator;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingController {
     private final InputView inputView;
 
     public RacingController(InputView inputView) {
         this.inputView = inputView;
+    }
 
+    public void run() {
+        List<Name> names = carNamesFromInput();
+        TryCount tryCount = tryCountFromInput();
+
+        Cars cars = Cars.createCarList(names);
+
+        playGame(cars, tryCount);
+
+        OutputView.printWinners(cars);
+    }
+
+    private void playGame(Cars cars, TryCount tryCount) {
+        for(int i = 0; i < tryCount.getCount(); i++) {
+            cars.moveAllCars();
+            OutputView.printCarStatus(cars);
+        }
+    }
+
+    private List<Name> carNamesFromInput() {
+        String input = inputView.inputCarNames();
+        return InputParser.inputParseDelimiter(input);
+    }
+
+    private TryCount tryCountFromInput() {
+        int count = inputView.inputTryCount();
+        return new TryCount(count);
     }
 }
